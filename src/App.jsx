80bab41cb63e9d9c15bf9b1c57e173451396c809
Leovita777 <141558126+Leovita777@users.noreply.github.com>
2024-03-26 23:08:31 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default App;
